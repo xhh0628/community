74040294330407ff1317c3db25fac7d2e04bac46
xhh1 <1947817805@qq.com>
2021-02-02 21:41:25 +0800

    init repo

---
+++ src/main/java/com/xhh_study1/community/controller/TestHelloController.java
@@ -0,0 +1,18 @@
+package com.xhh_study1.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestHelloController {
+
+        @GetMapping("/testSpringbootStart")
+        public String testSpringbootStart(@RequestParam(name="name", required=false, defaultValue="World?") String name, Model model) {
+            model.addAttribute("name", name);
+            return "testSpringbootStart";
+        }
+
+
+}
