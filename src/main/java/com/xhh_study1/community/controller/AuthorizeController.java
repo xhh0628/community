57b66e464b7b088e6edeb86b2d0082cadb6291e7
xhh1 <1947817805@qq.com>
2021-02-13 17:58:39 +0800

    20210213

--- src/main/java/com/xhh_study1/community/controller/AuthorizeController.java
+++ src/main/java/com/xhh_study1/community/controller/AuthorizeController.java
@@ -1,5 +1,10 @@
 package com.xhh_study1.community.controller;
 
+import com.xhh_study1.community.dto.AccessTokenDto;
+import com.xhh_study1.community.dto.GithubUser;
+import com.xhh_study1.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +12,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeController {
 
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private  String clientId;
+    @Value("${github.client.secret}")
+    private  String clientSecret;
+    @Value("${github.redirect.uri}")
+    private  String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
 
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setState(state);
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        String accessToken=githubProvider.getAccessToken(accessTokenDto);
+        GithubUser githubUser=githubProvider.getUser(accessToken);
+        System.out.println(githubUser);
         return "index";
     }
 }
---
+++ src/main/java/com/xhh_study1/community/dto/AccessTokenDto.java
@@ -0,0 +1,12 @@
+package com.xhh_study1.community.dto;
+
+import lombok.Data;
+
+@Data
+public class AccessTokenDto {
+    private String client_id;
+    private String client_secret;
+    private String code;
+    private String redirect_uri;
+    private String state;
+}
